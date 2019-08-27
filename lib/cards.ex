3dfdeb48bad450198932463537d05fde208a20c2
Gabriel Henrique <gabrieldesousa.h@gmail.com>
2019-08-27 19:39:48 -0300

    iniciando com Elixir

---
+++ lib/cards.ex
@@ -0,0 +1,11 @@
+defmodule Cards do
+
+  def hello do
+    "hi there!"
+    # Elixir has a implicitr return function
+  end
+  # To compile all files with elix, use "iex" - Interactive elixir shell
+  # iex -S mix
+  # Use "Cards" to call the Cards's module
+  # And Cards.hello to call the last function
+end
